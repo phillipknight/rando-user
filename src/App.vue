1215177ba149d4290ed290f2962f0e0f5ffa3541
phillipknight <phillipkknight@gmail.com>
2019-08-31 13:43:31 -0400

    Step 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <people-list></people-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PeopleList  from './components/PeopleList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    PeopleList
   }
 }
 </script>
---
+++ src/components/PeopleList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>In Person List</h2>
+    <div v-if="people.length">
+      <div v-for="person in people" :key="person.id.value">
+        <img :src="person.picture.medium" />
+        {{ person.name.first }} {{ person.name.last }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PeopleList',
+  data() {
+    return {
+      people: []
+    };
+  },
+  async mounted () {
+  fetch("https://randomuser.me/api/?results=10")
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      this.people = result.results;
+    });
+}
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
